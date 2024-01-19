88f71184bb92cfc5b781ce2c5a774d2661fffb24
Rohit Kumar <rk2613026@gmail.com>
2024-01-19 12:19:02 -0800

    React Router

--- src/assets/components/Contact/Contact.jsx
+++ src/assets/components/Contact/Contact.jsx
@@ -38,7 +38,7 @@ export default function Contact() {
                                     />
                                 </svg>
                                 <div className="ml-4 text-md tracking-wide font-semibold w-40">
-                                    Acme Inc, Street, State, Postal Code
+                                    Faridabad, Haryana
                                 </div>
                             </div>
 
@@ -60,7 +60,7 @@ export default function Contact() {
                                     />
                                 </svg>
                                 <div className="ml-4 text-md tracking-wide font-semibold w-40">
-                                    +44 1234567890
+                                    +91 9821976564
                                 </div>
                             </div>
 
@@ -82,7 +82,7 @@ export default function Contact() {
                                     />
                                 </svg>
                                 <div className="ml-4 text-md tracking-wide font-semibold w-40">
-                                    info@acme.org
+                                    letberohit@gmail.com
                                 </div>
                             </div>
                         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,46 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './assets/components/Home/Home.jsx'
+import About from './assets/components/About/About.jsx'
+import Contact from './assets/components/Contact/Contact.jsx'
+import User from './assets/components/User/User.jsx'
+import Github from './assets/components/Github/Github.jsx'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '',
+        element: <Home />
+      }, 
+      {
+        path: 'about',
+        element: <About />
+      },
+      {
+        path: 'contact',
+        element: <Contact />
+      },
+      {
+        path: 'user/:userid',
+        element: <User />
+      },
+      {
+        path: 'github',
+        element: <Github />
+      }
+    ]
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
